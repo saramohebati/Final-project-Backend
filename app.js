29631861d15863e690e0f85bb6f73ef236b52509
sara <sara.mohebati71@gmail.com>
2022-07-23 10:09:41 +0430

    Add first version of user router to app

--- app.js
+++ app.js
@@ -11,4 +11,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/users', userRouter);
+
 module.exports = app;
