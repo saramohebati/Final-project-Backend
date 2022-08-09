3817876251a2094ebd86ac67c78feafddc6fcaae
sara <sara.mohebati71@gmail.com>
2022-08-09 09:52:03 +0430

    Add CORS package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 const userRouter = require('./src/module/users/router.js');
 const AuthMiddleware = require('./src/core/middleware/auth');
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({origin: ["http://localhost:3000"]}));
 
 app.use('/users', userRouter);
 app.post('/login', AuthMiddleware.login);
